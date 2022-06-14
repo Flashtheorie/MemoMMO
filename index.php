a529c62b95949b574aca7c911e69364b5be786ac
FLashtheorie <antoinepascualmail@gmail.com>
2022-06-14 11:02:11 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MemoMMO</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <div id="title"><h1>MemoMMO</h1></div>
+    
+
+<div id="buttonPlay" align="center">
+    <button class="pushable">
+ 
+  <span class="front">
+   PLAY
+  </span>
+ 
+</button>
+</div>
+
+<script type="text/javascript" src="scripts/app.js"></script>
+</body>
+</html>
