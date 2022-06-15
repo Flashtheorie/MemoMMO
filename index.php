9c98c148e53d9d85a72e3bd3f2c412ad4c34f83d
Flashtheorie <antoinepascualmail@gmail.com>
2022-06-15 12:40:41 +0200

    Nettoyage index

--- index.php
+++ index.php
@@ -10,78 +10,29 @@
 <body>
 
 <div id="game">
+
+<div id="title" class="noselect"><h1>MEMOMMO</h1></div>
 <div id="secondTitle">The first RPG Memory Game </div>
-    <div id="title" class="noselect"><h1>MEMOMMO</h1></div>
-    
-    <div id="buttonPlay" align="center">
-    <button class="btn" id="play">
- 
-  
-   PLAY
-  
- 
-</button>
+
+<div id="buttonPlay" align="center">
+   <button class="btn" id="play">
+      PLAY
+   </button>
 
 </div>
-
-
-
-
-
 </div>
 
 <div id="gameboard">
 
-
-
-
-
-
 <h1>0/100 xp</h1>
 <div id="xpbar"></div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<table id="table">
-        <tr>
-            <th class="tiles"id = "tileUn"></th>
-            <th class="tiles"id = "tileDeux"></th>
-            <th class="tiles"id = "tileTrois"></th>
-            <th class="tiles" id = "tileQuatre"></th>
-        </tr>
-        <tr>
-            <th class="tiles" id = "tileCinq"></th>
-            <th class="tiles" id = "tileSix"></th>
-            <th class="tiles" id = "tileSept"></th>
-            <th class="tiles"id = "tileHuit"></th>
-        </tr>
-        <tr>
-            <th class="tiles" id = "tileNeuf"></th>
-            <th class="tiles" id = "tileDix"></th>
-            <th class="tiles" id = "tileOnze"></th>
-            <th class="tiles" id="tileDouze"></th>
-        </tr>
-
-        <tr>
-            <th class="tiles "id="tileTreize"></th>
-            <th class="tiles " id="tileQuatorze"></th>
-            <th class="tiles "id="tileQuinze"></th>
-            <th class="tiles "id="tileSeize"></th>
-        </tr>
-    </table>
 </div>
+
+
+
+
+<script type="text/javascript" src="timer/app.js"></script>
 <script type="text/javascript" src="scripts/app.js"></script>
 </body>
 </html>
